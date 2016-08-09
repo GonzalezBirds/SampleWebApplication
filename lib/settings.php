d1721ceef499ff5f6f6936498fc18b47e66818bb
Anna Kruglyanskaya <anna.kruglyanskaya@creditcards.com>
2016-08-09 15:14:30 -0500

    Getting files put together

---
+++ lib/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * settings.php
+ * 
+ * configuration settings for the Login Script.
+ */
+
+// Database Settings //////////////////////////////////////////////////////////
+
+	$dbhost =		"localhost";					//MySQL Host
+	$dbname =		"";								//MySQL DB name
+	$dbuser =		"";								//MySQL User
+	$dbpassword =	"";								//MySQL password
+	
+////////////////////////////////////////////////////////////////////////////////
+
+?>
